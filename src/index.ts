06b0f3ca7277e621e41f1492fc28b5c460d6f51c
MannRobertJ <mann.robertj@gmail.com>
2019-02-12 14:39:49 +0100

    done i guess

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import "reflect-metadata";
-import { createKoaServer } from "routing-controllers";
+import { createKoaServer, Action } from "routing-controllers";
 import PageController from "./pages/controller";
+import UserController from "./users/controller";
+import setupDb from "./db";
+import { verify } from "./logins/jwt";
+import LoginController from "./logins/controller";
 
 const app = createKoaServer({
-  controllers: [PageController]
+  controllers: [PageController, UserController, LoginController],
+  authorizationChecker: (action: Action) => {
+    const header: string = action.request.headers.authorization;
+
+    if (header && header.startsWith("Bearer ")) {
+      const [, token] = header.split(" ");
+      return !!(token && verify(token));
+    }
+    return false;
+  }
 });
 
-app.listen(4000, () => console.log("4000"));
+setupDb()
+  .then(_ => app.listen(4000, () => console.log("Listening on port 4000")))
+  .catch(err => console.error(err));
--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -5,37 +5,37 @@ import {
   Post,
   Param,
   Body,
-  HttpCode
+  HttpCode,
+  NotFoundError,
+  Authorized
 } from "routing-controllers";
-import pagesById, { Page } from "./data";
-
-type PageList = { pages: Page[] };
+import Page from "./entity";
 
 @JsonController()
 export default class PageController {
   @Get("/pages/:id")
-  getPage(@Param("id") id: number): Page {
-    return pagesById[id];
+  getPage(@Param("id") id: number) {
+    return Page.findOne(id);
   }
 
   @Get("/pages")
-  allPages(): PageList {
-    const pageArray: Page[] = Object.keys(pagesById).map(
-      page => pagesById[page]
-    );
-    return { pages: pageArray };
+  async allPages() {
+    const pages = await Page.find();
+    return { pages };
   }
 
+  @Authorized()
   @Put("/pages/:id")
-  updatePage(@Param("id") id: number, @Body() body: Partial<Page>): Page {
-    console.log(`Incoming PUT body param:`, body);
-    return pagesById[id];
+  async updatePage(@Param("id") id: number, @Body() update: Partial<Page>) {
+    const page = await Page.findOne(id);
+    if (!page) throw new NotFoundError("Cannot find page");
+    return Page.merge(page, update).save();
   }
 
+  @Authorized()
   @Post("/pages")
   @HttpCode(201)
-  createPage(@Body() body: Page): Page {
-    console.log(`Incoming POST body param:`, body);
-    return body;
+  createPage(@Body() page: Page) {
+    return page.save();
   }
 }
